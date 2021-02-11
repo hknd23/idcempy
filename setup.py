113ba797f174909fc8db69f13bc4b4cff07a32be
hknd23 <35204932+hknd23@users.noreply.github.com>
2021-02-10 23:55:02 -0500

    reformat setup

--- setup.py
+++ setup.py
@@ -3,31 +3,30 @@ from os import path
 from setuptools import find_packages, setup
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='idcempy',
-    packages=['idcempy'],
-    version='0.0.1',
-    packages=find_packages(),
-    license='mit',
-    description='Inflated Discrete Choice Models',
+    name="idcempy",
+    packages=find_packages(where="idcempy"),
+    version="0.0.1",
+    license="mit",
+    description="Inflated Discrete Choice Models",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Nguyen Huynh, Sergio Bejar, Vineeta Yadav, Bumba Mukherjee',
-    author_email='nguyenhuynh831@gmail.com',
-    url='https://github.com/hknd23/idcempy',
-    keywords=['Inflated', 'Mixture', 'Ordered Probit', 'Multinomial Logit'],
+    long_description_content_type="text/markdown",
+    author="Nguyen Huynh, Sergio Bejar, Vineeta Yadav, Bumba Mukherjee",
+    author_email="nguyenhuynh831@gmail.com",
+    url="https://github.com/hknd23/idcempy",
+    keywords=["Inflated", "Mixture", "Ordered Probit", "Multinomial Logit"],
     install_requires=[
-        'scipy',
-        'numpy',
-        'pandas'
+        "scipy",
+        "numpy",
+        "pandas"
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
-        'Programming Language :: Python :: 3.7',
-        'Intended Audience :: Science/Research',
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Science/Research",
     ],
 )
