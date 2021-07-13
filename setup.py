5d9f8841c7e47fa52cf8b184adaaf0cc9a632b78
hknd23 <35204932+hknd23@users.noreply.github.com>
2021-07-13 15:26:52 -0400

    update setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 setup(
     name="idcempy",
     packages=find_packages(where="idcempy"),
-    version="0.1.0",
+    version="0.1.1",
     license='GPLv3',
     description="Inflated Discrete Choice Models",
     long_description=long_description,
